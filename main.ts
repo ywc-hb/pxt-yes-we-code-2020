2c16c5449854ba29346aa93debe4ed5ced850368
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2019-12-11 17:32:54 +0100

    2e test

--- main.ts
+++ main.ts
@@ -1,7 +1,4 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//Coucou tout le monde, c'est un test
+
+LCD1IN8.LCD_Init()
+LCD1IN8.LCD_Clear()
