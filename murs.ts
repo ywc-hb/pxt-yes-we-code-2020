a033b9e7335116150e9f33a2b6ec0a972d42e485
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2019-12-14 18:09:51 +0100

    1er pattern

--- murs.ts
+++ murs.ts
@@ -1,6 +1,10 @@
 function patternOne() :void {
     /* Fonction dessinant les murs de la map 1 */
-    
+    let murs = [[0, 30, 15, 30]] //Coordonnées des murs au format [[x_start, y_start, x_end, y_end], ...]
+
+    for(let i = 0; i < murs.length ; i++) {
+        
+    }
 }
 
 function patternTwo() :void {
@@ -17,7 +21,7 @@ function displayWalls() :void {
     /* Fonction qui choisit aléatoirement une map et qui l'affiche */
 
     let pattern_number = Math.randomRange(1, 3);
-
+/*
     switch(pattern_number) {
         case 1:
             patternOne();
@@ -35,6 +39,7 @@ function displayWalls() :void {
             patternOne();
             break;
     }
-
+*/
+    patternOne();
     LCD1IN8.LCD_Display();
 }
