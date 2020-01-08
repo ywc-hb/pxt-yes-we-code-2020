3b43ce34db3ca1b1b91cfd4693c23d98268433fc
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-01-08 17:36:24 +0100

    Ajout setters et getters à Character

--- character.ts
+++ character.ts
@@ -14,9 +14,61 @@ class Character {
         this._color = color;
     }
 
+    //------------------- Setters and getters ----------------------
+
+    get name(): string {
+        return this._name;
+    }
+    
+    get death(): number {
+        return this._death;
+    }
+
+    get x(): number {
+        return this._x;
+    }
+
+    get y(): number {
+        return this._y;
+    }
+
+    get color(): number {
+        return this._color;
+    }
+
+    set x(x_new: number) {
+        if(x_new <= 0 || x_new > 160) {
+            basic.showIcon(IconNames.Angry);
+            basic.pause(100);
+            basic.showNumber(200);
+        }
+
+        else {
+            this._x = x_new;
+        }
+    }
+
+    set y(y_new: number) {
+        if (y_new <= 0 || y_new > 128) {
+            basic.showIcon(IconNames.Angry);
+            basic.pause(100);
+            basic.showNumber(200);
+        }
+
+        else {
+            this._y = y_new;
+        }
+    }
+
+    set death(add: number) {
+        this._death += add;
+    }
+
+    //------------------- End of setters and getters ----------------------
+
     displayCharacter() {
-        LCD1IN8.DrawCircle(this._x, this._y, 3, this._color, DRAW_FILL.DRAW_FULL, DOT_PIXEL.DOT_PIXEL_1);
-        LCD1IN8.LCD_DisplayWindows(this._x - 4, this._y - 4, this._x + 4, this._y + 4);
+        LCD1IN8.DrawCircle(this.x, this.y, 3, this.color, DRAW_FILL.DRAW_FULL, DOT_PIXEL.DOT_PIXEL_1);
+        LCD1IN8.LCD_DisplayWindows(this.x - 4, this.y - 4, this.x + 4, this.y + 4);
     }
 
     move(x_vector: number, y_vector: number) {
