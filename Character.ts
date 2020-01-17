69fbb86b1c4c06ef35e7a7c2c0a229f878424925
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-01-17 18:17:10 +0100

    Que retourner si code pour revivre erroné ?

--- Character.ts
+++ Character.ts
@@ -1,9 +1,11 @@
 class Character extends Jouabilite {
     protected _death: number;
+    protected _reviveCode: Image[];
 
     constructor(name: string, x: number, color: number, pattern: Walls) {
         super(name, x, 64, color, 3, pattern);
         this._death = 0;
+        this.reviveCode(false);
     }
 
     //------------------- Setters and getters ----------------------
@@ -25,15 +27,35 @@ class Character extends Jouabilite {
         this.y += vectors.y_vector;
         this.displayCharacter(this.color);
     }
-    public reviveCode(action: boolean) {
+    public reviveCode(action: boolean, list_items?: number[]): number | void {
         //'action vaut 'false' s'il faut donner le code et 'true' s'il faut le tester
-        let fleche = images.createImage(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
+
+        let list_possibilities = [images.arrowImage(ArrowNames.North), images.arrowImage(ArrowNames.South), images.arrowImage(ArrowNames.East), images.arrowImage(ArrowNames.West), 
+            images.iconImage(IconNames.Square), images.iconImage(IconNames.Triangle)]
+        
+        if (!action) {
+            this._reviveCode = [];
+            for (let i = 0; i < (this.death + 1) * 2; i++) {
+                this._reviveCode.push(list_possibilities[Math.randomRange(0, list_possibilities.length)]);
+                this._reviveCode[i].showImage(0)
+                basic.pause(1000 / (this.death + 1))
+            }
+        }
+
+        else {
+            for (let i = 0; i < list_items.length; i++) {
+                if (list_possibilities[list_items[i]] === this._reviveCode[i]) {
+                    continue
+                }
+                else {
+                    images.iconImage(IconNames.Sad);
+                    basic.pause(1);
+                    return(1); // Voir quoi retourner si le code est erroné...
+                }
+            }
+        }
+
+
     }
 
 }
