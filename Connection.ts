31256cef273f7c09c19c1334e67319f7d35a55e0
ywc-hb <58783406+ywc-hb@users.noreply.github.com>
2020-01-23 16:20:18 +0100

    constructor

--- Connection.ts
+++ Connection.ts
@@ -1,4 +1,10 @@
 // Ajouter votre code ici
 class Connection {
+    protected _channel: number;
 
+    constructor(channel: number) {
+        this._channel = channel;
+        radio.setGroup(this._channel);
+        radio.setTransmitPower(255);
+    }
 }
